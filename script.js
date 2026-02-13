6f8e5327591b87f1e59fedb27c95a1c11b19ab8e
Haroon <haroonbhai24680@gmail.com>
2026-02-13 23:20:40 +0500

    update

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("redBtn").onclick = function () {
+  document.body.style.backgroundColor = "red";
+};
+
+document.getElementById("blueBtn").onclick = function () {
+  document.body.style.backgroundColor = "blue";
+};
+
+document.getElementById("greenBtn").onclick = function () {
+  document.body.style.backgroundColor = "green";
+};
+
+document.getElementById("orangeBtn").onclick = function () {
+  document.body.style.backgroundColor = "orange";
+};
+
+document.getElementById("resetBtn").onclick = function () {
+  document.body.style.backgroundColor = "white";
+};
