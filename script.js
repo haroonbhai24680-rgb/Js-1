97fc95820d0b577e2ed0c839db04008806eb81aa
Haroon <haroonbhai24680@gmail.com>
2026-06-26 16:09:58 +0500

    update

--- script.js
+++ script.js
@@ -1,19 +1,66 @@
-document.getElementById("redBtn").onclick = function () {
-  document.body.style.backgroundColor = "red";
-};
+const apiKey = "b22a88f326e77505054a34442d4791e2";
 
-document.getElementById("blueBtn").onclick = function () {
-  document.body.style.backgroundColor = "blue";
-};
+const searchBtn = document.getElementById("searchBtn");
+const cityInput = document.getElementById("city");
 
-document.getElementById("greenBtn").onclick = function () {
-  document.body.style.backgroundColor = "green";
-};
+async function getWeather(city) {
 
-document.getElementById("orangeBtn").onclick = function () {
-  document.body.style.backgroundColor = "orange";
-};
+    const url =
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-document.getElementById("resetBtn").onclick = function () {
-  document.body.style.backgroundColor = "white";
-};
+    try{
+
+        const response = await fetch(url);
+
+        if(!response.ok){
+            alert("city not found");
+            return;
+        }
+
+        const data = await response.json();
+
+        document.getElementById("temp").innerHTML =
+        Math.round(data.main.temp) + "°C";
+
+        document.getElementById("cityName").innerHTML =
+        data.name;
+
+        document.getElementById("description").innerHTML =
+        data.weather[0].description;
+
+        document.getElementById("humidity").innerHTML =
+        data.main.humidity + "%";
+
+        document.getElementById("wind").innerHTML =
+        data.wind.speed + " km/h";
+
+        document.getElementById("icon").src =
+        "https://openweathermap.org/img/wn/" +
+        data.weather[0].icon +
+        "@2x.png";
+
+    }catch(error){
+        alert("Something went wrong.");
+    }
+}
+
+searchBtn.addEventListener("click",()=>{
+
+    const city = cityInput.value.trim();
+
+    if(city===""){
+        alert("Enter a city name");
+        return;
+    }
+
+    getWeather(city);
+
+});
+
+cityInput.addEventListener("keypress",(e)=>{
+
+    if(e.key==="Enter"){
+        searchBtn.click();
+    }
+
+});
